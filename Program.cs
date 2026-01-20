25194d6764d55249cd3a2bc8842bf064c418e58b
Andreas Hovaldt Højrup <andreas.hovaldt@gmail.com>
2026-01-20 16:42:49 +0100

    Added window scaling and removed old code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 namespace SandSimulator;
 
@@ -28,41 +29,17 @@ internal static class Program
     [System.STAThread]
     public static void Main()
     {
-        int windowWidth = 800;
-        int windowHeight = 480;
+        // Window settings
+        int windowWidth = 400;
+        int windowHeight = 250;
+        float windowScale = 4.0f;
 
         // Initilize a flat color array for texture data 
         Color[] colorArray = new Color[windowWidth * windowHeight];
         Array.Fill(colorArray, Color.Red);
 
-        // for (int x = 0; x < windowWidth; x += 2)  // Every other column (0, 2, 4, ...)
-        // {
-        //     for (int y = 0; y < windowHeight; y++)
-        //     {
-        //         int idx = y * windowWidth + x;  // Row-major: y * width + x
-        //         colorArray[idx] = Color.White;
-        //     }
-        // }
-
-
-        // Color[][] colorGrid = new Color[windowWidth][];
-        // for (int i = 0; i < colorGrid.Length; i++)
-        // {
-        //     colorGrid[i] = new Color[windowHeight];
-        //     Array.Fill(colorGrid[i], Color.Red);
-        // }
-
-        // Color[,] colorArray2 = new Color[windowWidth, windowHeight];
-        // for (int i = 0; i < windowWidth; i++)
-        // {
-        //     for (int j = 0; j < windowHeight; j++)
-        //     {
-        //         colorArray2[i,j] = Color.White;
-        //     }
-        // }
-
         // Initilize raylib window
-        Raylib.InitWindow(width: windowWidth, height: windowHeight, title: "Sand Simulator");
+        Raylib.InitWindow(width: (int)(windowWidth * windowScale), height: (int)(windowHeight * windowScale), title: "Sand Simulator");
         Raylib.SetTargetFPS(fps: 200);
 
         // Initilize blank image and load as Texture2D
@@ -82,7 +59,7 @@ internal static class Program
 
             // Simulation vizualization
             Raylib.BeginDrawing();
-            Raylib.DrawTexture(texture: windowTexture, posX: 0, posY: 0, tint: Color.White);
+            Raylib.DrawTextureEx(texture: windowTexture, position: new Vector2(0, 0), rotation: 0.0f, scale: windowScale, tint: Color.White);
 
             // Display runtime info
             Raylib.DrawRectangle(10, 10, 154, 47, Color.LightGray);
